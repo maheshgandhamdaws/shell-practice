ade8b56490742f5d0c894fcd20eeefeed322b2ef
gandhammahesh <maheshgandham214@gmail.com>
2025-06-10 19:01:28 +0530

    hello world started

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-NUMBER1=100
+NUMBER1=gandhammahesh
 NUMBER2=200
 
 TIMESTAMP=$(date)
 echo "Script executed at: $TIMESTAMP"
 SUM=$(($NUMBER1+$NUMBER2))
 
-echo "SUM of gandhammahesh and $NUMBER2 is: $SUM"
+echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
